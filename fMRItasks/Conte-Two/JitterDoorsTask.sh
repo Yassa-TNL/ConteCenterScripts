a81b25744ae2f6e5d98f3cdeda4810a0c565b744
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-07-25 16:57:49 -0700

    Calculates Optimal Jitter For Events From The Doors Task

--- fMRItasks/Conte-Two/JitterDoorsTask.sh
+++ fMRItasks/Conte-Two/JitterDoorsTask.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 ###################################################################################################
 ##########################              CONTE Center 2.0                 ##########################
@@ -21,13 +20,8 @@ Use
 module load afni/v19.0.01
 
 /dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fMRItasks/Conte-Two/optseq2 --ntp 110 --tr 2 \
---psdwin 0 20 2 --ev WIN 6 11 --ev LOSS 6 11  --tprescan -10 --evc 1 -1 --nkeep 3 --o Doors --tnullmin 2 \
---tnullmax 10 --nsearch 1000
-
-
-/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fMRItasks/Conte-Two/optseq2 --ntp 110 --tr 2 \
---psdwin 0 20 2 --ev WIN 6 13 --ev LOSS 6 13  --tprescan -12 --evc 1 -1 --nkeep 3 --o TRY2 --tnullmin 2 \
---tnullmax 10 --nsearch 1000
+--psdwin 0 20 0.5 --ev WIN 6 13 --ev LOSS 6 13 --tprescan -6 --evc 1 -1 --nkeep 3 --o Doors --tnullmin 0.5 \
+--tnullmax 6.5 --nsearch 1000000
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
