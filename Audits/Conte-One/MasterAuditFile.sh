9faf60985be5c86db90e33c1940010bb184ff1cf
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-07-31 01:52:11 -0700

    Creates Master Audit File for All MRI Data

--- Audits/Conte-One/MasterAuditFile.sh
+++ Audits/Conte-One/MasterAuditFile.sh
@@ -4,7 +4,7 @@
 ##########################              Robert Jirsaraie                 ##########################
 ##########################              rjirsara@uci.edu                 ##########################
 ###################################################################################################
-#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
 ###################################################################################################
 <<Use
 
@@ -14,7 +14,7 @@ google docs for all researchers to access
 
 Use
 ###################################################################################################
-#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
 ###################################################################################################
 
 module load afni/v19.0.01
@@ -51,20 +51,19 @@ echo 'subid,Session,ScanDate' > ${FINAL_OUTPUT}
 cat ${dir_temp}/TEMP_MRI_Master_Audit.txt >> ${FINAL_OUTPUT}
 echo '564,3,NA' >> ${FINAL_OUTPUT}
 sort -k1 -t ',' -g ${FINAL_OUTPUT} -o  ${FINAL_OUTPUT}
-chmod ug+wrx $FINAL_OUTPUT
-
 rm ${dir_temp}/TEMP_MRI*
+dos2unix ${FINAL_OUTPUT}
+chmod ug+wrx $FINAL_OUTPUT
 
 ###############################################################
 ### Add Columns Indicating if DICOMS and PARREC Files Exist ###
 ###############################################################
 
-header_og=`echo subid,Session,ScanDate`
-header_new=`echo subid,Session,ScanDate,Dicoms,PARREC`
+header_og=`head -n1 ${FINAL_OUTPUT}`
+header_new=`echo ${header_og},Dicoms,PARREC`
 cat ${FINAL_OUTPUT} | sed s@"${header_og}"@"${header_new}"@g > ${FINAL_OUTPUT}_NEW
 mv ${FINAL_OUTPUT}_NEW ${FINAL_OUTPUT}
 
-dos2unix ${FINAL_OUTPUT}
 rows=`cat ${FINAL_OUTPUT} | grep -v 'subid' | tr '\n' ' '`
 for row in $rows ; do
   subid=`echo $row | cut -d ',' -f1`
@@ -84,7 +83,6 @@ for row in $rows ; do
   fi
 done
 
-dos2unix ${FINAL_OUTPUT}
 rows=`cat ${FINAL_OUTPUT} | grep -v 'subid' | tr '\n' ' '`
 for row in $rows ; do
   subid=`echo $row | cut -d ',' -f1`
@@ -92,7 +90,7 @@ for row in $rows ; do
 
   dir_parrec=`echo /dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-One/${subid}_*_${session}/PARREC`
   if [ -d "${dir_parrec}" ]; then
-    echo 'PARREC files Detected For Subject '${subid}' and Session '${session}
+    echo 'PARREC Files Detected For Subject '${subid}' and Session '${session}
     newrow=`echo ${row},1`
     cat ${FINAL_OUTPUT} | sed s@"${row}"@"${newrow}"@g > ${FINAL_OUTPUT}_NEW
     mv ${FINAL_OUTPUT}_NEW ${FINAL_OUTPUT}
@@ -105,12 +103,38 @@ for row in $rows ; do
 done
 chmod ug+wrx ${FINAL_OUTPUT}
 
-###############################################################
-### Add Columns Indicating if DICOMS and PARREC Files Exist ###
-###############################################################
+############################################################################
+### Add Column of Scan Date Based on Dicom Headers to Varify Source File ###
+############################################################################
 
+header_og=`head -n1 ${FINAL_OUTPUT}`
+header_new=`echo ${header_og},FileDate`
+cat ${FINAL_OUTPUT} | sed s@"${header_og}"@"${header_new}"@g > ${FINAL_OUTPUT}_NEW
+mv ${FINAL_OUTPUT}_NEW ${FINAL_OUTPUT}
 
+rows=`cat ${FINAL_OUTPUT} | grep -v 'subid' | tr '\n' ' '`
+for row in $rows ; do
+  subid=`echo $row | cut -d ',' -f1`
+  session=`echo $row | cut -d ',' -f2`
 
+  dir_dicom=`echo /dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-One/${subid}_*_${session}/DICOMS`
+  if [ -d "${dir_dicom}" ]; then
+  	dcm=`ls /dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-One/${subid}_*_${session}/DICOMS/* | head -n1`
+  	header=`dicom_hdr ${dcm} | grep 'ID Study Date'`
+    date=`echo ${header: -8}`
+    newrow=`echo ${row},${date}`
+    cat ${FINAL_OUTPUT} | sed s@"${row}"@"${newrow}"@g > ${FINAL_OUTPUT}_NEW
+    mv ${FINAL_OUTPUT}_NEW ${FINAL_OUTPUT}
+    echo 'Scan Completed On '${date}' of Session '${session}' For Subject '${subid}
+  else
+    newrow=`echo $row,NA`
+    cat ${FINAL_OUTPUT} | sed s@"${row}"@"${newrow}"@g> ${FINAL_OUTPUT}_NEW
+    mv ${FINAL_OUTPUT}_NEW ${FINAL_OUTPUT}
+    echo 'DICOMS Missing For Subject '${subid}' and Session '${session}
+  fi
+done
+chmod ug+wrx ${FINAL_OUTPUT}
 
-
-
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+###################################################################################################
