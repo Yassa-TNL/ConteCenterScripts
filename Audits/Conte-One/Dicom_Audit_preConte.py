1f2b80bfa9ae323d6fd80070a7516cf27444e898
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-08-07 19:10:23 -0700

    Aggregates Missing Dicoms from Baseline TP:0

--- Audits/Conte-One/Dicom_Audit_preConte.py
+++ Audits/Conte-One/Dicom_Audit_preConte.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/python
 ###################################################################################################
 ##########################              CONTE Center 1.0                 ##########################
 ##########################              Robert Jirsaraie                 ##########################
@@ -7,22 +7,17 @@
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
 ###################################################################################################
 
-import os 
-
-with open("/dfs2/yassalab/rjirsara/ConteCenter/Audits/Conte-One/Audit_Master_ConteMRI.csv", "r") as audit:
-	csv=audit.read()
-print(csv)
-
-
-####
-
 import pandas as pd
+import nibabel as nb
+import numpy as np
+import shutil as cp
 import os
-import pathlib
-import glob  
-from shutil import copyfile
+import os.path
+import glob 
 
-
+###########################################
+### Copy Files Into Temp Data Structure ###
+###########################################
 
 audit = pd.read_csv('/dfs2/yassalab/rjirsara/ConteCenter/Audits/Conte-One/Audit_Master_ConteMRI.csv')
 base = audit.loc[audit['Session'] == 0]
@@ -31,83 +26,48 @@ base_maxrows = base.shape[0]
 for row in range(base_maxrows):
 	subid = base.iloc[row][0]
 	MRIses = base.iloc[row][1]
-	input_files = '/dfs2/yassalab/ConteCenter/1point0/preConte/LACIE_SHARE/all-Nifti_files_asof_sept21-09/{}*'.format(subid)
+	dir_output = '/dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-PreOne/{}_0_{}/NIFTIS'.format(subid,MRIses)
+    	if not os.path.isdir(dir_output):
+        	os.makedirs(dir_output)
+	input_files = '/dfs2/yassalab/ConteCenter/1point0/preConte/LACIE_SHARE/all-Nifti_files_asof_sept21-09/{}.*'.format(subid)
 	input_files = glob.glob(input_files)
-	dir_output = '/dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-PreOne/{}_0_{}'.format(subid,MRIses)
-	pathlib.Path(dir_output).mkdir(parents=True, exist_ok=True)
-		os.makedirs(dir_output)
+	subid
+	if input_files != "":
+		for FILE in input_files:
+			cp.copy(FILE, dir_output)
+			print('Copying File: {}'.format(FILE))
 
+##############################
+### Convert Files to NIFTI ###
+##############################
 
+images = '/dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-PreOne/*/*/*.img'
+images = glob.glob(images)
 
+for image in images:
+	fname = image  
+	img = nb.load(fname)
+	nb.save(img, fname.replace('.img', '.nii.gz'))
+	print('Coverting this image: {}'.format(image))
 
+#################################################
+### Copy Existing Files to Aggregated Dataset ###
+#################################################
 
+NIFTIS = '/dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-PreOne/*/*/*.nii.gz'
+NIFTIS = glob.glob(NIFTIS)
 
-	for file in input_files:
-		copyfile(file, dir_output)
+for nii in NIFTIS:
+	subid = os.path.basename(nii)
+	subid = os.path.splitext(subid)[0]
+ 	subid = os.path.splitext(subid)[0]
+	PATH='/dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-One/{}_0_0/NIFTIS/'.format(subid)
+	if not os.path.isdir(PATH):
+        	os.makedirs(PATH)
+	OUTPUT='{}/sub-{}_ses-0_T1w.nii.gz'.format(PATH,subid)
+	cp.copy(nii, OUTPUT)
+	print('Finalizing for Subject: {}'.format(subid))
 
-
-
-	copyfile(input_files, dir_output)
-
-
-	
-
-
-
-
-
-
-subid
-	session
-
-
-
-"this {}is {}some {}string!".format(1,2,3)
-
-maxrow
-
-
-
-
-row_count = sum(for row in audit)
-
-
-
-
-
-
-
-for row in audit[0:maxrow]:
-	print(row)
-
-
-
-
-
-df.iloc[3:6] 
-
-
-
-
-
-
-
-
-audit=open("/dfs2/yassalab/rjirsara/ConteCenter/Audits/Conte-One/Audit_Master_ConteMRI.csv")
-text=audit.read()
-
-print(text)
-
-
-
-
-
-
-
-
-
-
-
-
-
-os.getcwd()
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+###################################################################################################
