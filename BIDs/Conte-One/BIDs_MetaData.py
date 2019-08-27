d16b818b37861edab8fdaf815f4f5f1dcde72a00
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-08-26 18:21:53 -0700

    Appends Missing MetaData From Philips Scanner To JSON Files

--- BIDs/Conte-One/BIDs_MetaData.py
+++ BIDs/Conte-One/BIDs_MetaData.py
@@ -17,14 +17,13 @@ files that were outputed during the BIDs Conversion.
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
 ###################################################################################################
 
+from collections import OrderedDict
 from __future__ import division
-import numpy as np
-import shutil as cp
 import os
 import os.path
 import glob
 import operator
-import pandas as pd
+import json
 
 #######################################################
 ### Locate Json Files of Functional Images Per Task ###
@@ -35,24 +34,12 @@ def LocateFiles(TaskName):
         json = glob.glob(json)
         return json
 
-JSONS_Hippocampus=LocateFiles('HIPP')
-JSONS_Resting=LocateFiles('REST')
-JSONS_Amygdala=LocateFiles('AMG')
-
-###########################
-### Relabel Tasks Names ###
-###########################
-
-
-#####################################################
-### Add Phase Direction Information AP: j, PA: -j ###
-#####################################################
-
-"PhaseEncodingDirection": "j",
-"PhaseEncodingDirection": "j",
+JSONS_REST=LocateFiles('REST')
+JSONS_HIPP=LocateFiles('HIPP')
+JSONS_AMG=LocateFiles('AMG')
 
 ###############################################################
-###    Slice Timing Information Based on Scanner Protocol   ###
+###            Define Slice Timing Information              ###
 ### 51/42 Slices, TR: 2, Default Slice Order/Single Package ###
 ###############################################################
 
@@ -105,12 +92,43 @@ def DefaultSliceTiming(TRsec,nSlices):
     return(final)
 
 
-STI_Resting=DefaultSliceTiming(2,51)
-STI_Hippocampus=DefaultSliceTiming(2,51)
-STI_Amygdala=DefaultSliceTiming(2,42)
+STI_REST=DefaultSliceTiming(2,51)
+STI_HIPP=DefaultSliceTiming(2,51)
+STI_AMG=DefaultSliceTiming(2,42)
+
+###########################################
+### Append New Information in Json File ###
+###  Phasing Direction: AP = j RL = i   ###
+###########################################
+
+def AppendJsonFiles(JSONS,STI,taskname,PhaseEncod):
+	for singlefile in JSONS:
+		Content=json.load(open(singlefile), object_pairs_hook=OrderedDict)
+		if 'SliceTiming' in Content.keys():
+			print ''
+  			print "Information Already Appended for Following File:"
+			print singlefile
+		else:
+			software=Content['ConversionSoftware']
+			del Content['ConversionSoftware']
+			version=Content['ConversionSoftwareVersion']
+			del Content['ConversionSoftwareVersion']
+			Content["TaskName"] = taskname
+			Content["SliceTiming"] = STI
+			Content["PhaseEncodingDirection"] = PhaseEncod #AP= j RL= i
+			Content["ConversionSoftware"] = software
+			Content["ConversionSoftwareVersion"] = version
+			Content["ImageType"]=['ORIGINAL','PRIMARY','M','FFE','M','FFE']
+			print "Outputing File:"			
+			print(singlefile)
+			print(Content)			
+			with open(singlefile, "w") as write_file:
+    				json.dump(Content, write_file, indent=12)
 
 
-
+AppendJsonFiles(JSONS_REST,STI_REST,'REST','j')
+AppendJsonFiles(JSONS_HIPP,STI_HIPP,'HIPP','i')
+AppendJsonFiles(JSONS_AMG,STI_AMG,'AMG','j')
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
