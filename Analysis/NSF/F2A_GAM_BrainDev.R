5e67c20e74ceedeec68a4772de4ade274f87a564
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-09-06 17:32:26 -0700

    working

--- Analysis/NSF/F2A_GAM_BrainDev.R
+++ Analysis/NSF/F2A_GAM_BrainDev.R
@@ -14,64 +14,6 @@ library(RColorBrewer)
 library(visreg)
 library(stringr)
 
-
-suppressMessages(require(ggplot2))
-suppressMessages(require(base))
-suppressMessages(require(reshape2))
-suppressMessages(require(nlme))
-suppressMessages(require(lme4))
-suppressMessages(require(gamm4))
-suppressMessages(require(stats))
-suppressMessages(require(knitr))
-suppressMessages(require(mgcv))
-suppressMessages(require(plyr))
-suppressMessages(require(oro.nifti))
-suppressMessages(require(parallel))
-suppressMessages(require(optparse))
-suppressMessages(require(fslr))
-suppressMessages(require(voxel))
-suppressMessages(require(stringr))
-suppressMessages(require(ggplot2))
-suppressMessages(require(base))
-suppressMessages(require(reshape2))
-suppressMessages(require(nlme))
-suppressMessages(require(lme4))
-suppressMessages(require(gamm4))
-suppressMessages(require(stats))
-suppressMessages(require(knitr))
-suppressMessages(require(mgcv))
-suppressMessages(require(plyr))
-suppressMessages(require(oro.nifti))
-suppressMessages(require(parallel))
-suppressMessages(require(optparse))
-suppressMessages(require(fslr))
-suppressMessages(require(voxel))
-suppressMessages(require(stringr))
-############################################
-#### Gather ARI RESULTS To be Published ####
-############################################
-
-#Pull p-values# ENSURE IT IS THE CORRECT COVARIATE
-p_ari <- sapply(NmfModels, function(v) summary(v)$p.table[5,4])
-#Convert to data frame
-p_ari <- as.data.frame(p_ari)
-#Print original p-values to three decimal places
-p_ari_round <- round(p_ari,3)
-#FDR correct p-values
-p_ari_fdr <- p.adjust(p_ari[,1],method="fdr")
-#Convert to data frame
-p_ari_fdr <- as.data.frame(p_ari_fdr)
-#To print fdr-corrected p-values to three decimal places
-p_ari_fdr_round <- round(p_ari_fdr,3)
-#List the NMF components that survive FDR correction
-Nmf_ARI_fdr <- row.names(p_ari_fdr)[p_ari_fdr<0.05]
-#Name of the NMF components that survive FDR correction
-Nmf_ARI_fdr_names <- nmfComponents[as.numeric(Nmf_ARI_fdr)]
-#To check direction of coefficient estimates
-ARI_coeff <- models[as.numeric(Nmf_ARI_fdr)]
-
-
-
 #######################################################################
 ##### Prepare the Data to Create Figure of Sample Characteristics #####
 #######################################################################
@@ -88,16 +30,10 @@ Results <- lapply(Models, summary)
 
 maxindex<-length(Models)
 
-for (x in 1:maxindex){
-  plotname<-paste("plot",x)
-  plotname<-visreg(Models[[x]],'AgeAtScan')
-}
-
-
-
-################################################
-### PLOT ARI as a Predictor of CT Network 18 ### 
-################################################
+#for (x in 1:maxindex){
+#  plotname<-paste("plot",x)
+#  plotname<-visreg(Models[[x]],'AgeAtScan')
+#}
 
 plotdata <- visreg(Models[[18]],'AgeAtScan',type = "conditional",scale = "linear", plot = FALSE)
 smooths <- data.frame(Variable = plotdata$meta$x,
@@ -129,39 +65,55 @@ Limbic<-ggplot() +
 ggsave(file="/data/jux/BBL/projects/jirsaraieStructuralIrrit/output/NMF_Figures/F4_Rate/ScatterPlot_ctRATE_Network18.pdf", device = "pdf", width = 4, height = 5.5)
 
 ###############################################
-### PLOT ARI as a Predictor of CT Network 1 ### 
+###  ### 
 ###############################################
 
-plotdata <- visreg(NmfModels[[9]],'ari_log',type = "conditional",scale = "linear", plot = FALSE)
-smooths <- data.frame(Variable = plotdata$meta$x,
-                      x=plotdata$fit[[plotdata$meta$x]],
-                      smooth=plotdata$fit$visregFit,
-                      lower=plotdata$fit$visregLwr,
-                      upper=plotdata$fit$visregUpr)
+allscans<-read.csv("/dfs2/yassalab/rjirsara/NSF/Data/COVA-n275_Age+Sex_20190829_RESP-n360_Aseg_volume_20190829/AllScans/n268_gamm4_sAgeAtScank4-Gender_random_1sub_Merged.csv")
 
-predicts <- data.frame(Variable = "dim1",
-                       x=plotdata$res$ari_log,
-                       y=plotdata$res$visregRes)
+BrainRegions <- names(allscans)[6:67]
 
-colkey <- "#2ebeff"
-lineColor<- "#2ebeff"
-p_text <- "p[fdr] == 0.008"
+Models <- lapply(BrainRegions, function(x) {
+  gam(substitute(i ~ s(AgeAtScan, k=4) + Gender, list(i = as.name(x))), method="REML", data = baseline)
+})
 
-Limbic<-ggplot() +
-  geom_point(data = predicts, aes(x, y, colour = x), alpha= 1  ) +
-  scale_colour_gradientn(colours = colkey,  name = "") +
-  geom_line(data = smooths, aes(x = x, y = smooth), colour = lineColor,size=2) +
-  geom_line(data = smooths, aes(x = x, y=lower), linetype="dashed", colour = lineColor, alpha = 0.9, size = 1.5) +
-  geom_line(data = smooths, aes(x = x, y=upper), linetype="dashed",colour = lineColor, alpha = 0.9, size = 1.5) +
-  annotate("text",x = -Inf, y = Inf, hjust = -0.1,vjust = 1,label = p_text, parse=TRUE,size = 8, colour = "black",fontface ="italic" ) +
-  theme(legend.position = "none") +
-  labs(x = "", y = "") +
-  theme(axis.title=element_text(size=26,face="bold"), axis.text=element_text(size=14), axis.title.x=element_text(color = "black"), axis.title.y=element_text(color = "black"))
+Results <- lapply(Models, summary)
 
-### Save Scatterplot ###
+gamm4(formula = i ~ s(AgeAtScan, k = 4) + Gender, list(i = as.name(x))), random=as.formula(randomFormula), data=dataSubj, REML=T)$gam
 
-ggsave(file="/data/jux/BBL/projects/jirsaraieStructuralIrrit/output/NMF_Figures/F4_Rate/ScatterPlot_ctRATE_Network9.pdf", device = "pdf", width = 4, height = 5.5)
 
+
+
+ggplot(allscans, aes(x=AgeAtScan, y=, group=sub, colour=sub + geom_line(size=.3) + geom_smooth(aes(group=Group), method="loess", size=2, se=F)+ geom_smooth(aes(group=Group), method="lm", size=1, se=F)
+
+
+
+
+
+
+
+
+############################################
+#### Gather ARI RESULTS To be Published ####
+############################################
+
+#Pull p-values# ENSURE IT IS THE CORRECT COVARIATE
+p_ari <- sapply(NmfModels, function(v) summary(v)$p.table[5,4])
+#Convert to data frame
+p_ari <- as.data.frame(p_ari)
+#Print original p-values to three decimal places
+p_ari_round <- round(p_ari,3)
+#FDR correct p-values
+p_ari_fdr <- p.adjust(p_ari[,1],method="fdr")
+#Convert to data frame
+p_ari_fdr <- as.data.frame(p_ari_fdr)
+#To print fdr-corrected p-values to three decimal places
+p_ari_fdr_round <- round(p_ari_fdr,3)
+#List the NMF components that survive FDR correction
+Nmf_ARI_fdr <- row.names(p_ari_fdr)[p_ari_fdr<0.05]
+#Name of the NMF components that survive FDR correction
+Nmf_ARI_fdr_names <- nmfComponents[as.numeric(Nmf_ARI_fdr)]
+#To check direction of coefficient estimates
+ARI_coeff <- models[as.numeric(Nmf_ARI_fdr)]
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
 ###################################################################################################
