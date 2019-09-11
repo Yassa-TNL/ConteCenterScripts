2ee3e235afba9ede4b916841e87ecbf34a650066
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-09-11 12:01:09 -0700

    Execute Fmriprep Preprocessing Pipeline

--- fmriprep/Conte-One/fmriprep_preproc_pipeline.sh
+++ fmriprep/Conte-One/fmriprep_preproc_pipeline.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
-#$ -N FSUBxSES
-#$ -q yassalab,pub*
-#$ -pe openmp 8-64
+#$ -q yassalab,free*
+#$ -pe openmp 16
 #$ -R y
 #$ -ckpt restart
 #####################################
-### Define Input and Output Paths ###
+### Load Software & Define Inputs ###
 #####################################
 
-scripts_path=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fmriprep/Conte-One
-bids_root_path=/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One
-output_inter_dir=/dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Spooling/One
-log_dir=`echo $output_inter_dir/fmriprep/logs`
-mkdir -p $log_dir
+module purge 2>/dev/null 
+module load singularity/3.0.0 2>/dev/null 
 
-#############################################################
-### Execute Fmriprep Pipeline using Singularity Container ###
-#############################################################
+sub=`echo $1`
+fmriprep_container=`echo $2`
+bids_directory=/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One
+fslicense=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fmriprep/Conte-One/fs_license.txt
 
-singularity run --cleanenv CONTAINER ${bids_root_path} ${output_inter_dir} participant --participant_label SUB --fs-license-file \
-${scripts_path}/fs_license.txt --fs-no-reconall --longitudinal --force-bbr --use-aroma --fd-spike-threshold 0.2 --use-syn-sdc \
---write-graph --low-mem > ${log_dir}/SUBxSES_stdout.txt 2>&1
+##############################################################################
+### Execute Subject-Level Analysis of FMRIPREP Using Singularity Container ###
+##############################################################################
 
-######################################################
-### Move Output to Final Subject-level Directories ###
-######################################################
+output_dir=/dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One/sub-${sub}
+working_dir=${output_dir}/sub-${sub}_intermediates
+commandfile=`echo ${output_dir}/logs/sub-${sub}_command.txt`
+logfile=`echo ${output_dir}/logs/sub-${sub}_stdout.txt`
+mkdir -p ${working_dir} `dirname ${logfile}` 
+rm FP${sub}.e* FP${sub}.o* 
 
-if [ -f ${output_inter_dir}/fmriprep/sub-SUB*.html ] ; then
+echo " singularity run --cleanenv ${fmriprep_container} ${bids_directory} participant --participant_label ${sub} -w ${output_inter_dir} --fs-license-file ${scripts_path}/fs_license.txt --fs-no-reconall --longitudinal --force-bbr --use-aroma --fd-spike-threshold 0.2 --use-syn-sdc --write-graph --low-mem" > ${commandfile}
 
-  chmod -r 775 ${output_inter_dir}/fmriprep/sub-SUB*
-  cp -rf ${output_inter_dir}/fmriprep/sub-SUB* /dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One/ 
-  cp -rf ${log_dir}/SUBxSES_stdout.txt /dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One/logs/
-
-else
-
-  echo ''
-  echo "${SUB}x${SES} did not complete processing through the fmriprep pipeline - Check ogs"
-  echo ''
-
-fi
+singularity run --cleanenv ${fmriprep_container} \
+  ${bids_directory} \
+  ${output_dir} \
+  participant --participant_label ${sub} \
+  --work-dir ${working_dir} \
+  --fs-license-file ${fslicense} \
+  --skip-bids-validation \
+  --fs-no-reconall \ 
+  --longitudinal \
+  --nthreads 16 \
+  --use-aroma \
+  --fd-spike-threshold 0.2 \
+  --use-syn-sdc \
+  --write-graph \
+  --stop-on-first-crash \
+  --low-mem > ${logfile} 2>&1
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
