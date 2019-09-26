cbdb9cf1b23875844c2f55b6c81ce658d5d51517
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-09-26 15:27:45 -0700

    Execute MRIQC Pipeline

--- mriqc/Conte-One/mriqc_pipeline.sh
+++ mriqc/Conte-One/mriqc_pipeline.sh
@@ -21,27 +21,27 @@ bids_directory=/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One
 
 if [ ! ${sub} == "GROUP" ]; then
 
-output_dir=/dfs2/yassalab/rjirsara/ConteCenter/mriqc/Conte-One
-working_dir=${output_dir}/sub-${sub}_ses-${ses}_intermediates
-commandfile=`echo ${output_dir}/logs/sub-${sub}_ses-${ses}_command.sh`
-logfile=`echo ${output_dir}/logs/sub-${sub}_ses-${ses}_stdout.txt`
-mkdir -p ${working_dir} `dirname ${logfile}` 
-rm QC${sub}x${ses}.e* QC${sub}x${ses}.o* 
+  output_dir=/dfs2/yassalab/rjirsara/ConteCenter/mriqc/Conte-One
+  working_dir=${output_dir}/sub-${sub}_ses-${ses}_intermediates
+  commandfile=`echo ${output_dir}/logs/sub-${sub}_ses-${ses}_command.sh`
+  logfile=`echo ${output_dir}/logs/sub-${sub}_ses-${ses}_stdout.txt`
+  mkdir -p ${working_dir} `dirname ${logfile}` 
+  rm QC${sub}x${ses}.e* QC${sub}x${ses}.o* 
 
-echo "singularity run --cleanenv ${mriqc_container} ${bids_directory} ${output_dir} participant --participant-label ${sub} --session-id ${ses} --work-dir ${working_dir} --n_procs 8 --ants-nthreads 8 --fft-spikes-detector --fd_thres 0.2" > ${commandfile}
+  echo "singularity run --cleanenv ${mriqc_container} ${bids_directory} ${output_dir} participant --participant-label ${sub} --session-id ${ses} --work-dir ${working_dir} --n_procs 8 --ants-nthreads 8 --fft-spikes-detector --fd_thres 0.2" > ${commandfile}
 
-singularity run --cleanenv ${mriqc_container} \
-  ${bids_directory} \
-  ${output_dir} \
-  participant --participant-label ${sub} \
-  --session-id ${ses} \
-  --work-dir ${working_dir} \
-  --n_procs 8 \
-  --ants-nthreads 8 \
-  --fft-spikes-detector \
-  --fd_thres 0.2 > ${logfile} 2>&1
+  singularity run --cleanenv ${mriqc_container} \
+    ${bids_directory} \
+    ${output_dir} \
+    participant --participant-label ${sub} \
+    --session-id ${ses} \
+    --work-dir ${working_dir} \
+    --n_procs 8 \
+    --ants-nthreads 8 \
+    --fft-spikes-detector \
+    --fd_thres 0.2 > ${logfile} 2>&1
 
-chmod -R 775 ${output_dir}
+  chmod -R 775 ${output_dir}
 fi
 
 ##########################################################
@@ -50,25 +50,23 @@ fi
 
 if [ ${sub} == "GROUP" ]; then
 
-bids_directory=`echo /dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One`
-output_dir=`echo /dfs2/yassalab/rjirsara/ConteCenter/mriqc/Conte-One`
-working_dir=${output_dir}/Group_intermediates
-commandfile=`echo ${output_dir}/logs/Group_command.sh`
-logfile=`echo ${output_dir}/logs/Group_stdout.txt`
-mkdir -p ${working_dir} `dirname ${logfile}` 
-rm QC_GROUP.e* QC_GROUP.o* 
+  bids_directory=`echo /dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One`
+  output_dir=`echo /dfs2/yassalab/rjirsara/ConteCenter/mriqc/Conte-One`
+  commandfile=`echo ${output_dir}/logs/group_command.sh`
+  logfile=`echo ${output_dir}/logs/group_stdout.txt`
+  mkdir -p ${working_dir} `dirname ${logfile}` 
+  rm QC_GROUP.e* QC_GROUP.o* 
 
-echo "singularity run --cleanenv ${mriqc_container} ${bids_directory} ${output_dir} group --work-dir ${working_dir} --n_procs 8 --ants-nthreads 8" > ${commandfile}
+  echo "singularity run --cleanenv ${mriqc_container} ${bids_directory} ${output_dir} group --n_procs 8 --ants-nthreads 8" > ${commandfile}
 
-singularity run --cleanenv ${mriqc_container} \
-  ${bids_directory} \
-  ${output_dir} \
-  group \
-  --work-dir ${working_dir} \
-  --n_procs 8 \
-  --ants-nthreads 8 > ${logfile} 2>&1
+  singularity run --cleanenv ${mriqc_container} \
+    ${bids_directory} \
+    ${output_dir} \
+    group \
+    --n_procs 8 \
+    --ants-nthreads 8 > ${logfile} 2>&1
 
-chmod -R 775 ${output_dir}
+  chmod -R 775 ${output_dir}
 fi
 
 ###################################################################################################
