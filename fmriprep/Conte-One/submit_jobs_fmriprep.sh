c754ed7127aa59b9db3b714fe2696cad900d65bf
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-09-27 15:21:51 -0700

    Identify New Subjects For FMRIPREP PreProcessing Pipeline

--- fmriprep/Conte-One/submit_jobs_fmriprep.sh
+++ fmriprep/Conte-One/submit_jobs_fmriprep.sh
@@ -44,15 +44,15 @@ fi
 ##### Define New Subjects to be Processed and Submit Jobs #####
 ###############################################################
 
-AllSubs=`ls -d1 /dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One/sub-*/ses-*/func | cut -d '/' -f8 | cut -d '-' -f2 | head -n5`
+AllSubs=`ls -d1 /dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One/sub-*/ses-*/func | cut -d '/' -f8 | cut -d '-' -f2 | head -n25`
 
 for sub in ${AllSubs} ; do
 
   output_base_dir=/dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One
 
-  qa=`echo ${output_base_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_bold_confounds.tsv | head -n1`
-  preproc=`echo ${output_base_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_*_preproc.nii | head -n1`
-  html=`echo ${output_base_dir}/fmriprep/sub-${sub}*.html | head -n1`
+  qa=`echo ${output_base_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_desc-confounds_regressors.tsv | cut -d ' ' -f1`
+  preproc=`echo ${output_base_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_*_desc-brain_mask.nii.gz | cut -d ' ' -f1`
+  html=`echo ${output_base_dir}/fmriprep/sub-${sub}*.html`
 
   if [ -f ${qa} ] && [ -f ${preproc} ] && [ -f ${html} ] ; then
 
@@ -64,7 +64,7 @@ for sub in ${AllSubs} ; do
 
   else
 
-    jobstats=`qstat -u $USER | grep FP${sub} | awk {'print $5'}`
+    job=`qstat -u $USER | grep FP${sub} | awk {'print $5'}`
 
     if [ "$job" == "r" ] || [ "$job" == "Rr" ] || [ "$job" == "Rq" ] || [ "$job" == "qw" ] ; then
 
