53425c669f1c10f6ca725ed2ab2b17a54263ffb7
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-10-07 22:33:10 -0700

    test

--- xcpEngine/Conte-One/xcpEngine_postproc_pipeline.sh
+++ xcpEngine/Conte-One/xcpEngine_postproc_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -q yassalab,free*
-#$ -pe openmp 16
+#$ -pe openmp 4
 #$ -R y
 #$ -ckpt restart
 #####################################
@@ -10,27 +10,27 @@
 module purge 2>/dev/null 
 module load singularity/3.0.0 2>/dev/null 
 
-sub=105
-xcp_container=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/xcpEngine/xcpEngine-latest.simg
-designs_file=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/xcpEngine/Conte-One/designs/fc-36p.dsn
-xcp_outputdir=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One
+sub=$1
+ses=$2
+scan=$3
+xcpEngine_container=$4
+seq=`echo $scan | cut -d '_' -f3`
 
 #############################
 ### Define Tracking Files ###
 #############################
 
-commandfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_command.sh
-cohortfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_cohort.csv
-logfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_stdERR+stdOUT.txt
+commandfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_${ses}_${seq}_command.sh
+cohortfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_${ses}_${seq}_cohort.csv
+logfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_${ses}_${seq}_stdERR+stdOUT.txt
 
 mkdir -p ${xcp_outputdir} ${xcp_workdir} ${fmri_inputdir} 
-rm XCP${sub}.e* XCP${sub}.o* 
+rm ${sub}"${seq:5:1}"${ses}.e* ${sub}"${seq:5:1}"${ses}.o* 
 
 ##########################
 ### Create Cohort File ###
 ##########################
 
-scans=`echo /dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz`
 echo "id0,img" > $cohortfile
 
 for scan in $scans ; do 
@@ -41,17 +41,26 @@ done
 ### Execute xcpEngine Pipeline ###
 ##################################
 
-echo singularity run --cleanenv ${xcp_container} \
-  -c ${cohort_file} \
-  -d ${designs_file} \
-  -o ${xcp_outputdir} \
-  -t 3 > ${commandfile}
+designs_file=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/xcpEngine/Conte-One/fc-24p.dsn
 
-chmod -R 775 ${commandfile}
+for design in ${designs_file} ; do
 
-${commandfile} > ${logfile} 2>&1
+  option=`basename $designs_file | cut -d '.' -f1`
+  xcp_outputdir=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/${seq}/sub-${sub}/ses-${ses}/${option}
 
-chmod -R 775 ${xcp_outputdir}
+  echo singularity run --cleanenv ${xcpEngine_container} \
+    -c ${cohortfile} \
+    -d ${design} \
+    -o ${xcp_outputdir} \
+    -t 3 > ${commandfile}
+
+  chmod -R 775 ${commandfile}
+
+  ${commandfile} > ${logfile} 2>&1
+
+  chmod -R 775 ${xcp_outputdir}
+
+done
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
