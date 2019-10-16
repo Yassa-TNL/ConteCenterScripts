114ac112f0d969142160e1c251affd68ed5b8427
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-10-16 16:28:15 -0700

    Finds Newly Scanned Subjects To Curate Across Sites

--- BIDs/Conte-Two/PushPsychPyData.sh
+++ BIDs/Conte-Two/PushPsychPyData.sh
@@ -22,8 +22,8 @@ Use
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
 ###################################################################################################
 
-module load flywheel/8.5.0 
-source ~/MyPassCodes.txt
+export PATH=$PATH:/data/users/rjirsara/flywheel/linux_amd64
+source /data/users/rjirsara/MyPassCodes.txt
 fw login ${FLYWHEEL_API_TOKEN}
 
 ##############################################################
@@ -46,16 +46,11 @@ chmod -R ug+wrx ${output_dir}
 for file in $Files ; do
 
   name=`basename $file` 
-  subid=`echo $name | cut -d '_' -f1`
+  subid=`basename $name | cut -d '_' -f1`
   site_output=/dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-Two-UCI/BIDs/sub-${subid}/ses-1/func/
   bids_output=/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-${subid}/ses-1/func/
   event_output=/dfs2/yassalab/rjirsara/ConteCenter/Dicoms/Conte-Two-UCI/${subid}/EventFiles
-  EXISTING=`fw ls "yassalab/Conte-Two-UCI/${subid}/Brain^ConteTwo/files/${name}"`
 
-  if [ ! -z $EXISTING ] ; then
-    break
-  fi
-  
   if [ ! -d ${event_output} ] ; then
     mkdir ${event_output} 
   fi
@@ -87,8 +82,11 @@ for file in $Files ; do
 ### Upload Copies to Flywheel to be Archived ###
 ################################################
 
-  fw upload "yassalab/Conte-Two-UCI/${subid}/Brain^ConteTwo/" ${file}
+  EXISTING=`fw ls "yassalab/Conte-Two-UCI/${subid}/Brain^ConteTwo/files/${name}" | cut -d ' ' -f6`
 
+  if [ -z $EXISTING ] ; then
+    fw upload "yassalab/Conte-Two-UCI/${subid}/Brain^ConteTwo/" ${file}
+  fi
 done
 
 rm -rf ${output_dir}
