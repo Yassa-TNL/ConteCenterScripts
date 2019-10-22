13896e5adab2f1bb4814a87b3081364d80a8fd0c
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-10-21 19:55:56 -0700

    Reformat to Align with BIDs Formatting

--- BIDs/Conte-Two/BIDs_MetaData.py
+++ BIDs/Conte-Two/BIDs_MetaData.py
@@ -8,11 +8,12 @@ import os
 import os.path
 import shutil
 import glob
-import operator
 import json
 import sys
-import numpy as np
 import nipype
+import operator
+import subprocess
+import numpy as np
 from collections import OrderedDict
 from nipype.interfaces import afni, ants, fsl, utility
 from nipype.interfaces.fsl import Merge, Split, ExtractROI, ImageMeants
@@ -62,9 +63,9 @@ if JSONS == []:
 try:
 	FUNC = glob.glob('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/func/*.nii.gz'.format(SUBID,SITE))[0]
 except:
-	pass
+	FUNC = None
 
-if FUNC != []:
+if FUNC is not None:
 	Check=json.load(open(FUNC.replace("nii.gz","json")))
 	if Check["PhaseEncodingDirection"] == "j-":
 		OutInterBase="{}/SPLIT".format(os.path.dirname(FUNC))
@@ -76,7 +77,6 @@ if FUNC != []:
 		Seperate.run()
 		OutInterFiles=glob.glob("{}*0005.nii.gz".format(OutInterBase))
 		OutFinalFunc="/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/fmap/sub-{}_ses-{}_acq-dirAP_magnitude2.nii.gz".format(SUBID,SITE,SUBID,SITE)
-
 		merger = Merge()
 		merger.inputs.in_files=OutInterFiles
 		merger.inputs.dimension='t'
@@ -84,7 +84,6 @@ if FUNC != []:
 		merger.inputs.tr=1.5
 		merger.inputs.merged_file=OutFinalFunc
 		merger.run()
-
 		InputCopyJson=FUNC.replace(".nii.gz",".json")
 		OutFinalJson=OutFinalFunc.replace(".nii.gz",".json")
 		COPY=json.load(open(InputCopyJson), object_pairs_hook=OrderedDict)
@@ -92,7 +91,6 @@ if FUNC != []:
 		COPY["ProtocolName"] = "fmap-magnitude2_acq-dirPA"
 		with open(OutFinalJson, "w") as write_file:
 			json.dump(COPY, write_file, indent=12)
-
 		InterFILES=glob.glob("{}*.nii.gz".format(OutInterBase))
 		for file in InterFILES:
 			os.remove(file)
@@ -131,9 +129,9 @@ if any("_task-REST_" in s for s in FUNCS):
 try:
 	DWI = glob.glob('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/dwi/*.nii.gz'.format(SUBID,SITE))[0]
 except:
-	pass
+	DWI = None
 
-if DWI != []:
+if DWI is not None:
 	Check=json.load(open(DWI.replace("nii.gz","json")))
 	if Check["PhaseEncodingDirection"] == "j-":
 		OutFinalDWI=OutFinalFunc.replace("magnitude2.","magnitude1.")
@@ -174,9 +172,9 @@ if any("_run-01_" in s for s in DWIS):
 try:
 	MAG1 = glob.glob('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/fmap/*acq-dirPA_magnitude1.nii.gz'.format(SUBID,SITE))[0]
 except:
-	pass
+	MAG1 = None
 
-if MAG1 != []:
+if MAG1 is not None:
 	OutInterBase="{}/SPLIT".format(os.path.dirname(MAG1))
 	Seperate = fsl.Split(
 		in_file=MAG1,
@@ -184,7 +182,7 @@ if MAG1 != []:
 		output_type="NIFTI_GZ",
 		out_base_name=OutInterBase)
 	Seperate.run()
-	OUTPUT=glob.glob('{}000{}*'.format(OutInterBase,"5"))
+	OUTPUT=glob.glob('{}000{}*'.format(OutInterBase,"5"))[0]
 	os.rename(OUTPUT,MAG1)
 	INTERS=glob.glob('{}000*'.format(OutInterBase))
 	for INTER in INTERS:
@@ -193,9 +191,9 @@ if MAG1 != []:
 try:
 	MAG2 = glob.glob('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/fmap/*acq-dirPA_magnitude2.nii.gz'.format(SUBID,SITE))[0]
 except:
-	pass
+	MAG2 = None
 
-if MAG2  != []:
+if MAG2 is not None:
 	OutInterBase="{}/SPLIT".format(os.path.dirname(MAG2))
 	Seperate = fsl.Split(
 		in_file=MAG2,
@@ -203,9 +201,9 @@ if MAG2  != []:
 		output_type="NIFTI_GZ",
 		out_base_name=OutInterBase)
 	Seperate.run()
-	OUTPUT=glob.glob('{}000{}*'.format(OutInterBase,"5"))
+	OUTPUT=glob.glob('{}0004.nii.gz'.format(OutInterBase))[0]
 	os.rename(OUTPUT,MAG2)
-	INTERS=glob.glob('{}000*'.format(OutInterBase))
+	INTERS=glob.glob('{}000*.nii.gz'.format(OutInterBase))
 	for INTER in INTERS:
 		os.remove(INTER)
 
@@ -214,9 +212,9 @@ if MAG2  != []:
 #########################################################
 
 try:
-	EVENTS=glob.glob('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/func/*task-doors_events.tsv'.format(SUBID,SITE))[0]
+	EVENTS = glob.glob('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/func/*task-doors_events.tsv'.format(SUBID,SITE))[0]
 except:
-	pass
+	EVENTS = None
 
 RUNS=glob.glob('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}/ses-{}/func/*run-*_task-doors_bold.nii.gz'.format(SUBID,SITE))
 if (len(RUNS) == 3 and os.path.exists(EVENTS)):
@@ -242,9 +240,10 @@ if (len(RUNS) == 3 and os.path.exists(EVENTS)):
 		os.remove(nifti)
 		os.remove(json)
 
-if (len(RUNS) != 3 and os.path.exists(EVENTS)):
-	with open('/dfs2/yassalab/rjirsara/ConteCenter/Audits/Conte-Two/logs/ExtraRunErrors.csv', 'a') as file:
-		addrow="\n{},{},ExtraDoors".format(SUBID,SITE)
+if RUNS != []:
+	if (len(RUNS) != 3 and os.path.exists(EVENTS)):
+		with open('/dfs2/yassalab/rjirsara/ConteCenter/Audits/Conte-Two/logs/ExtraRunErrors.csv', 'a') as file:
+			addrow="\n{},{},ExtraDoors".format(SUBID,SITE)
 
 if any("_run-01_" in s for s in FUNCS):
 	with open('/dfs2/yassalab/rjirsara/ConteCenter/Audits/Conte-Two/logs/ExtraRunErrors.csv', 'a') as file:
@@ -270,10 +269,10 @@ if FMAPS != []:
 			Content["IntendedFor"]=[DWIS]
 			json.dump(Content, open(updatefile, "w"), indent=12)
 
-
-os.chmod('/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}'.format(SUBID), 0775)
-qsublogs=glob.glob("{}{}C.*".format(SUBID,SITE))
-#os.remove(qsublogs)
+subprocess.call(['chmod', '-R', 'ug+wrx', '/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-Two/sub-{}'.format(SUBID)])
+qsublogs=glob.glob("{}/{}{}C.*".format(os.getcwd(),SITE,SUBID))
+for qlog in qsublogs:
+	os.remove(qlog)
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
