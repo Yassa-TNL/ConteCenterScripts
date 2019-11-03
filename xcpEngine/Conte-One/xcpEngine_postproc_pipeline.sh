f368fc983f94de1f8091fbbeab579ffd6f592324
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-11-03 13:54:07 -0800

    Processes Incoming Scans Through Their Designated Pipelines

--- xcpEngine/Conte-One/xcpEngine_postproc_pipeline.sh
+++ xcpEngine/Conte-One/xcpEngine_postproc_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -q yassalab,free*
-#$ -pe openmp 4
+#$ -pe openmp 8
 #$ -R y
 #$ -ckpt restart
 #####################################
@@ -12,50 +12,61 @@ module load singularity/3.0.0 2>/dev/null
 
 sub=$1
 ses=$2
-scan=$3
+InputFile=$3
 xcpEngine_container=$4
-seq=`echo $scan | cut -d '_' -f3`
 
-#############################
-### Define Tracking Files ###
-#############################
+###########################################################
+### Determine Which Modules will be Run From Input File ###
+###########################################################
 
-commandfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_ses-${ses}_${seq}_command.sh
-cohortfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_ses-${ses}_${seq}_cohort.csv
-logfile=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/sub-${sub}_ses-${ses}_${seq}_stdERR+stdOUT.txt
+if [[ $InputFile == *"_bold.nii.gz" ]] ; then
 
-mkdir -p ${xcp_outputdir} ${xcp_workdir} ${fmri_inputdir} 
-rm echo "${seq:5:1}"${sub}X${ses}.e* echo "${seq:5:1}"${sub}X${ses}.o* 
+	designs_options="fc-36p fc-24p_gsr"
+	TASK=`echo $InputFile | cut -d '_' -f3 | cut -d '-' -f2`
 
-##########################
-### Create Cohort File ###
-##########################
+elif [[ $InputFile == *"_T1w.nii.gz" ]] ; then
 
-echo "id0,img" > $cohortfile
-echo "sub-${sub},${scan}" >> $cohortfile
+	designs_options="anat-antsct anat-complete anat_jlf_complete anat-minimal"
+	TASK=`echo $InputFile | cut -d '/' -f10`
+
+fi
+
+#############################################
+### Define Cohort, Command, and Log Files ###
+#############################################
+
+for DESIGN in ${designs_options} ; do
+
+	dir_root_logs=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/logs/pipe-${DESIGN}/task-${TASK}
+	commandfile=${dir_root_logs}/sub-${sub}_ses-${ses}_command.sh
+	cohortfile=${dir_root_logs}/sub-${sub}_ses-${ses}_cohort.csv
+	logfile=${dir_root_logs}/sub-${sub}_ses-${ses}_output.txt
+
+	rm "${TASK:0:2}"${sub}X${ses}.*
+	mkdir -p ${dir_root_logs}
+
+	echo "id0,id1,img" > $cohortfile
+	echo "sub-${sub},ses-${ses},${InputFile}" >> $cohortfile
 
 ##################################
 ### Execute xcpEngine Pipeline ###
 ##################################
 
-designs_file=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/xcpEngine/Conte-One/fc-24p.dsn
+	designfile=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/xcpEngine/Conte-One/designs/${DESIGN}.dsn
+	xcp_outputdir=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/pipe-${DESIGN}/task-${TASK}
+	mkdir -p ${xcp_outputdir}
 
-for design in ${designs_file} ; do
+ 	echo "singularity run --cleanenv ${xcpEngine_container} \
+		-c ${cohortfile} \
+		-d ${designfile} \
+		-o ${xcp_outputdir} \
+		-t 3" > ${commandfile}
+	
+	chmod -R 775 `dirname ${commandfile}`
 
-  option=`basename $designs_file | cut -d '.' -f1`
-  xcp_outputdir=/dfs2/yassalab/rjirsara/ConteCenter/xcpEngine/Conte-One/${seq}/sub-${sub}/ses-${ses}/${option}
+	${commandfile} > ${logfile} 2>&1
 
-  echo singularity run --cleanenv ${xcpEngine_container} \
-    -c ${cohortfile} \
-    -d ${design} \
-    -o ${xcp_outputdir} \
-    -t 3 > ${commandfile}
-
-  chmod -R 775 `dirname ${commandfile}`
-
-  ${commandfile} > ${logfile} 2>&1
-
-  chmod -R 775 ${xcp_outputdir}
+	chmod -R 775 ${xcp_outputdir}
 
 done
 
