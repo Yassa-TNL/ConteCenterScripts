c323da136cb29acee6abe5d2cc999e543d0d3f0c
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-12-01 17:02:17 -0800

    Execute Fmriprep Preprocessing Pipeline

--- fmriprep/Conte-One/fmriprep_preproc_pipeline.sh
+++ fmriprep/Conte-One/fmriprep_preproc_pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -q yassalab,free*
+#$ -q yassalab,free*,pub*
 #$ -pe openmp 16
 #$ -R y
 #$ -ckpt restart
@@ -10,44 +10,48 @@
 module purge 2>/dev/null 
 module load singularity/3.0.0 2>/dev/null 
 
-sub=`echo $1`
-fmriprep_container=`echo $2`
-bids_directory=/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One
-fslicense=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fmriprep/Conte-One/fs_license.txt
+sub=$1
+bids_root_dir=$2
+output_root_dir=$3
+Freesurfer_License=$4
+Singularity_Container=$5
 
-##############################################################################
-### Execute Subject-Level Analysis of FMRIPREP Using Singularity Container ###
-##############################################################################
+################################################
+### Prepare Log Files and Output Directories ###
+################################################
 
-output_dir=/dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One
-working_dir=${output_dir}/sub-${sub}_intermediates
-commandfile=`echo ${output_dir}/commands/sub-${sub}_command.sh`
-logfile=`echo ${output_dir}/commands/sub-${sub}_stdERR+stdOUT.txt`
-mkdir -p ${working_dir} `dirname ${logfile}` 
-rm FP${sub}.e* FP${sub}.o* 
+workflow_root_dir=${output_root_dir}/fmriprep/workflows
+commandfile=`echo ${output_root_dir}/logs/sub-${sub}_command.sh`
+logfile=`echo ${output_root_dir}/logs/sub-${sub}_output.txt`
+mkdir -p `dirname ${logfile}` ${working_dir}
+rm FPREP${sub}.*
 
-echo "singularity run --cleanenv ${fmriprep_container} ${bids_directory} ${output_dir} participant --participant_label ${sub} --work-dir ${working_dir} --fs-license-file ${fslicense} --skip-bids-validation --fs-no-reconall --longitudinal --nthreads 16 --use-aroma --fd-spike-threshold 0.2 --use-syn-sdc --write-graph --stop-on-first-crash --low-mem " > ${commandfile}
+#########################################################################
+### Execute FMRIPREP Using Singularity Container For A Single Subject ###
+#########################################################################
 
-singularity run --cleanenv ${fmriprep_container} \
-  ${bids_directory} \
-  ${output_dir} \
-  participant --participant_label ${sub} \
-  --work-dir ${working_dir} \
-  --fs-license-file ${fslicense} \
-  --skip-bids-validation \
-  --fs-no-reconall \
-  --longitudinal \
-  --nthreads 16 \
-  --use-aroma \
-  --fd-spike-threshold 0.2 \
-  --output-space fsaverage fsaverage5 fsnative template \
-  --template MNI152NLin2009cAsym \
-  --use-syn-sdc \
-  --write-graph \
-  --stop-on-first-crash \
-  --low-mem > ${logfile} 2>&1
+echo "singularity run --cleanenv ${Singularity_Container} \
+	${bids_root_dir} \
+	${output_root_dir} \
+	participant --participant_label ${sub} \
+	--work-dir ${workflow_root_dir} \
+	--fs-license-file ${Freesurfer_License} \
+	--skip-bids-validation \
+	--fs-no-reconall \
+	--longitudinal \
+	--n_cpus 16 \
+	--use-aroma \
+	--use-syn-sdc \
+	--write-graph \
+	--stop-on-first-crash \
+	--low-mem" | tr '\t' ' ' > ${commandfile}
 
-chmod -R 775 ${output_dir}
+chmod -R 775 `dirname ${commandfile}`
+
+${commandfile} > ${logfile} 2>&1
+
+chmod -R 775 ${output_root_dir}
+rm -rf ${workflow_root_dir}/fmriprep_wf/single_subject_${sub}_wf
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
