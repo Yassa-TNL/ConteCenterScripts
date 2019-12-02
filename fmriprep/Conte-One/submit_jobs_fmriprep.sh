5ada19560f2bc5a30de47b36c305c7349133f0e0
Robert Jisaraie <robertjirsaraie21@gmail.com>
2019-12-01 17:00:46 -0800

    Submits Newly Scanned Participants Through FMRIPREP Pipeline

--- fmriprep/Conte-One/submit_jobs_fmriprep.sh
+++ fmriprep/Conte-One/submit_jobs_fmriprep.sh
@@ -19,24 +19,32 @@ Use
 module purge 2>/dev/null 
 module load singularity/3.0.0 2>/dev/null 
 
+#########################################
+##### Define Input and Output Paths #####
+#########################################
+
+Singularity_Container=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fmriprep/fmriprep-latest.simg
+script_root_dir=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fmriprep/Conte-One
+bids_root_dir=/dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One
+output_root_dir=/dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One
+
 #######################################################
 ##### Build FMRIPREP Singularity Image if Missing #####
 #######################################################
 
-fmriprep_container=`echo /dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fmriprep/fmriprep-latest.simg`
-if [ -f $fmriprep_container ] ; then
+if [ -f $Singularity_Container ] ; then
 
-  version=`singularity run --cleanenv $fmriprep_container --version | cut -d ' ' -f2`
-  echo ''
-  echo "Preprocessing will be Completed using the fmriprep Singularity Image: ${version}"
-  echo ''
+	version=`singularity run --cleanenv $Singularity_Container --version | cut -d ' ' -f2`
+	echo ''
+	echo "Preprocessing will be Completed using the FmriPrep Singularity Image: ${version}"
+	echo ''
 
 else
 
-  echo ''
-  echo "Singularity Image Not Found -- Building New Containter with Latest Version of fmriprep"
-  echo ''
-  singularity build ${fmriprep_container} docker://poldracklab/fmriprep:latest
+	echo ''
+	echo "Singularity Image Not Found -- Building New Containter with Latest Version of fmriprep"
+	echo ''
+	singularity build ${Singularity_Container} docker://poldracklab/fmriprep:latest
 
 fi
 
@@ -44,49 +52,54 @@ fi
 ##### Define New Subjects to be Processed and Submit Jobs #####
 ###############################################################
 
-AllSubs=`ls -d1 /dfs2/yassalab/rjirsara/ConteCenter/BIDs/Conte-One/sub-*/ses-*/func | cut -d '/' -f8 | cut -d '-' -f2 | head -n25`
+AllSubjects=`ls -d1 ${bids_root_dir}/sub-* | xargs -n 1 basename | cut -d '-' -f2`
 
-for sub in ${AllSubs} ; do
+for sub in ${AllSubjects} ; do
 
-  output_base_dir=/dfs2/yassalab/rjirsara/ConteCenter/fmriprep/Conte-One
+	JobName=`echo FPREP${sub}`
+	JobStatus=`qstat -u $USER | grep "${JobName}\b" | awk {'print $5'}`
+	qa=`echo ${output_root_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_desc-confounds_regressors.tsv | cut -d ' ' -f1`
+	preproc=`echo ${output_root_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_*_desc-brain_mask.nii.gz | cut -d ' ' -f1`
+	html=`echo ${output_root_dir}/fmriprep/sub-${sub}*.html`
 
-  qa=`echo ${output_base_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_desc-confounds_regressors.tsv | cut -d ' ' -f1`
-  preproc=`echo ${output_base_dir}/fmriprep/sub-${sub}/ses-*/func/sub-${sub}_ses-*_task-*_*_desc-brain_mask.nii.gz | cut -d ' ' -f1`
-  html=`echo ${output_base_dir}/fmriprep/sub-${sub}*.html`
+	if [ ! -d `echo ${bids_root_dir}/sub-${sub}/ses-*/func | cut -d ' ' -f1` ] ; then
 
-  if [ -f ${qa} ] && [ -f ${preproc} ] && [ -f ${html} ] ; then
+		echo ''
+		echo "#####################################################"
+		echo "#sub-${sub} does not have functional scans to process"
+		echo "#####################################################"
+		echo ''
 
-    echo ''
-    echo "########################################################"
-    echo "#sub-${sub} already ran through the fmriprep pipeline..."
-    echo "########################################################"
-    echo ''
+	elif [ -f ${qa} ] && [ -f ${preproc} ] && [ -f ${html} ] ; then
 
-  else
+		echo ''
+		echo "#####################################################"
+		echo "#sub-${sub} already ran through the fmriprep pipeline"
+		echo "#####################################################"
+		echo ''
 
-    job=`qstat -u $USER | grep FP${sub} | awk {'print $5'}`
-    if [ ! -z "$jobstats" ] ; then
+	elif [ ! -z "$JobStatus" ] ; then
 
-       echo ''
-       echo "###########################################"
-       echo "#sub-${sub} is currently being processed..."
-       echo "###########################################"
-       echo ''
+		echo ''
+		echo "#######################################################"
+		echo "#sub-${sub} is currently being processed (${JobStatus})"
+		echo "#######################################################"
+		echo ''
 
-    else
+	else
 
-       echo ''
-       echo "##############################################"
-       echo "#Fmriprep Job Being Submitted for sub-${sub}  "
-       echo "##############################################"
-       echo ''
+		echo ''
+		echo "#######################################"
+		echo "#SUBMITTING FMRIPREP JOB FOR sub-${sub}"
+		echo "#######################################"
+		echo ''
 
-       JobName=`echo FP${sub}`
-       Pipeline=/dfs2/yassalab/rjirsara/ConteCenter/ConteCenterScripts/fmriprep/Conte-One/fmriprep_preproc_pipeline.sh
-       
-       qsub -N ${JobName} ${Pipeline} ${sub} ${fmriprep_container}
-    fi
-  fi
+		Pipeline=${script_root_dir}/fmriprep_preproc_pipeline.sh
+		Freesurfer_License=${script_root_dir}/freesurfer_license.txt
+
+		qsub -N ${JobName} ${Pipeline} ${sub} ${bids_root_dir} ${output_root_dir} ${Freesurfer_License} ${Singularity_Container}
+
+	fi
 done
 
 ###################################################################################################
