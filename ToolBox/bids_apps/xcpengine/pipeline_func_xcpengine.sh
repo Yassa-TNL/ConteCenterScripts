50714f4b237efc72ef9f3d0c3d00d9dee45b5cdc
Robert Jisaraie <robertjirsaraie21@gmail.com>
2020-04-29 14:45:07 -0700

    Change Output Directory Structure

--- ToolBox/bids_apps/xcpengine/pipeline_func_xcpengine.sh
+++ ToolBox/bids_apps/xcpengine/pipeline_func_xcpengine.sh
@@ -50,10 +50,10 @@ for PIPE in `echo $PIPELINES | tr '@' ' ' ` ; do
 		echo "id0,id1,id2,img" > ${COHORT_FILE}
 		HEADER_FORMAT='Longitudinal'
 	elif [[ $(echo $SCANS | tr ' ' '_') != *"ses-"* && $(echo $SCANS | tr ' ' '_') == *"run-"* ]] ; then
-		echo "id0,id1,id2,img" > ${COHORT_FILE}
+		echo "id0,id1,img" > ${COHORT_FILE}
 		HEADER_FORMAT='MultiRun'
 	elif [[ $(echo $SCANS | tr ' ' '_') == *"ses-"* && $(echo $SCANS | tr ' ' '_') == *"run-"* ]] ; then
-		echo "id0,id1,id2,id3,img" > ${COHORT_FILE}
+		echo "id0,id1,id2,img" > ${COHORT_FILE}
 		HEADER_FORMAT='Longitudinal_MultiRun'	
 	fi
 
@@ -70,26 +70,24 @@ for PIPE in `echo $PIPELINES | tr '@' ' ' ` ; do
 			if [[ $SCAN == *"run-"* ]] ; then
  				SUB=`basename $SCAN | cut -d '_' -f1 | cut -d '-' -f2`
  				RUN=`basename $SCAN | cut -d '_' -f3 | cut -d '-' -f2`
-				echo "sub-${SUB},task-${TASK},run-${RUN},${SCAN}" >> ${COHORT_FILE}
+				echo "sub-${SUB},task-${TASK}_run-${RUN},${SCAN}" >> ${COHORT_FILE}
 			else
 				SUB=`basename $SCAN | cut -d '_' -f1 | cut -d '-' -f2`
-				echo "sub-${SUB},task-${TASK},run-01,${SCAN}" >> ${COHORT_FILE}
+				echo "sub-${SUB},task-${TASK}_run-01,${SCAN}" >> ${COHORT_FILE}
 			fi
 		elif [[ $HEADER_FORMAT == "Longitudinal_MultiRun" ]] ; then
 			if [[ $SCAN == *"run-"*  ]] ; then
 				SUB=`basename $SCAN | cut -d '_' -f1 | cut -d '-' -f2`
  				SES=`basename $SCAN | cut -d '_' -f2 | cut -d '-' -f2`
  				RUN=`basename $SCAN | cut -d '_' -f4 | cut -d '-' -f2`
-				echo "sub-${SUB},ses-${SES},task-${TASK},run-${RUN},${SCAN}" >> ${COHORT_FILE}
+				echo "sub-${SUB},ses-${SES},task-${TASK}_run-${RUN},${SCAN}" >> ${COHORT_FILE}
 			else
 				SUB=`basename $SCAN | cut -d '_' -f1 | cut -d '-' -f2`
  				SES=`basename $SCAN | cut -d '_' -f2 | cut -d '-' -f2`
-				echo "sub-${SUB},ses-${SES},task-${TASK},run-01,${SCAN}" >> ${COHORT_FILE}
+				echo "sub-${SUB},ses-${SES},task-${TASK}_run-01,${SCAN}" >> ${COHORT_FILE}
 			fi
 		fi
 	done
-
-	echo ${SUBJECT},${TODAY},${HEADER_FORMAT} >> $DIR_OUTPUT_PATH/logs/directory_structure_${TASK}.csv
 
 ##################################
 ### Execute XCPEngine Pipeline ###
@@ -110,20 +108,19 @@ for PIPE in `echo $PIPELINES | tr '@' ' ' ` ; do
 
 	chmod -R 775 ${DIR_OUTPUT_PATH}/sub-${SUBJECT}
 	rmdir ${DIR_OUTPUT_PATH}/workflows/single_subject_${SUBJECT}/task-${TASK}
+	rmdir ${DIR_OUTPUT_PATH}/workflows/single_subject_${SUBJECT}
 
 ###################################################################################
 ### Extract Signal From Tissue Segmentations Following Demeaning and Detrending ###
 ###################################################################################
 
-	for DIR_ROOT in `find ${DIR_OUTPUT_PATH}/sub-${SUBJECT} -type f | grep task-${TASK} | sed s@"/task-${TASK}/"@','@g | cut -d ',' -f1 | uniq` ; do
-		for SEGMENT in `find $DIR_ROOT | grep prestats | grep segmentation.nii.gz` ; do 	
-			REGRESS_NIFTI=`echo $SEGMENT | sed s@'prestats'@'regress'@g | sed s@'segmentation'@'residualised'@g`					
-			EXTRACT_FILE=`echo $SEGMENT | sed s@'prestats'@'confound2'@g | sed s@'.nii.gz'@'.csv'@g`
-			singularity exec --cleanenv ${DIR_LOCAL_SCRIPTS}/container_xcpengine.simg \
-				/xcpEngine/utils/roi2ts.R \
-				-i ${REGRESS_NIFTI} \
-				-r ${SEGMENT} | sed s@' '@','@g > ${EXTRACT_FILE}
-		done
+	for SEGMENT_NIFTI in `find ${DIR_OUTPUT_PATH}/sub-${SUBJECT} -type f | grep task-${TASK} | grep prestats | grep segmentation.nii.gz` ; do
+		REGRESS_NIFTI=`echo $SEGMENT_NIFTI | sed s@'prestats'@'regress'@g | sed s@'segmentation'@'residualised'@g`					
+		EXTRACT_FILE=`echo $SEGMENT_NIFTI | sed s@'prestats'@'confound2'@g | sed s@'.nii.gz'@'.csv'@g`
+		singularity exec --cleanenv ${DIR_LOCAL_SCRIPTS}/container_xcpengine.simg \
+			/xcpEngine/utils/roi2ts.R \
+			-i ${REGRESS_NIFTI} \
+			-r ${SEGMENT_NIFTI} | sed s@' '@','@g > ${EXTRACT_FILE}
 	done
 
 ##########################################################
@@ -131,16 +128,18 @@ for PIPE in `echo $PIPELINES | tr '@' ' ' ` ; do
 ##########################################################
 
 	if [[ $HEADER_FORMAT == *"MultiRun"* ]] ; then
-		for DIR_ROOT in `find ${DIR_OUTPUT_PATH}/sub-${SUBJECT} -type f | grep task-${TASK} | sed s@"/task-${TASK}/"@','@g | cut -d ',' -f1 | uniq` ; do
+		for DIR_ROOT in `find ${DIR_OUTPUT_PATH}/sub-${SUBJECT} -type d | grep task-${TASK} | sed s@'task-'@'#'@g | cut -d '#' -f1| cut -d ',' -f1 | uniq` ; do
 			echo 'Merging Selected Files From Fcon Module'
-			for ATLAS in `ls ${DIR_ROOT}/task-${TASK}/run-01/fcon | grep -v .nii.gz` ; do
-				mkdir -p ${DIR_ROOT}/task-${TASK}/combine/fcon/${ATLAS}/
-				MERGE=`echo ${DIR_ROOT}/task-${TASK}/run-*/fcon/${ATLAS}/sub-${SUBJECT}*task-${TASK}_run-*_${ATLAS}_ts.1D`	
-				MERGED_FILE=`echo $MERGE | cut -d ' ' -f1 | sed s@run-01@'combine'@g` ; cat $MERGE > $MERGED_FILE
+			for ATLAS in `ls $(ls -d ${DIR_ROOT}/task-${TASK}_run-*/fcon  | tr '\n' ' ' | cut -d ' ' -f1) | grep -v .nii.gz` ; do
+				mkdir -p ${DIR_ROOT}/task-${TASK}_concat/fcon/${ATLAS}/
+				MERGE=`echo ${DIR_ROOT}/task-${TASK}_run-*/fcon/${ATLAS}/sub-${SUBJECT}*task-${TASK}_run-*_${ATLAS}_ts.1D`	
+				MERGED_FILE=`echo $MERGE | cut -d ' ' -f1 | sed s@run-01@'concat'@g` ; cat $MERGE > $MERGED_FILE
 				singularity exec --cleanenv ${DIR_LOCAL_SCRIPTS}/container_xcpengine.simg \
 					/xcpEngine/utils/ts2adjmat.R \
 					--ts $MERGED_FILE > `echo $MERGED_FILE | sed s@'ts.1D'@'network.txt'@g`
 			done
+
+
 			echo 'Merging Selected Files From Regress Module'
 			mkdir -p ${DIR_ROOT}/task-${TASK}/combine/regress
 			MERGE=`echo ${DIR_ROOT}/task-${TASK}/run-*/regress/sub-${SUBJECT}*task-${TASK}_run-*_nVolumesCensored.txt`	
