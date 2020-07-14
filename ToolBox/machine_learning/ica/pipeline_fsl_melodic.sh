9bf281f0dd7e56e7e19b4c6e172c2550d29b6f2c
Robert Jisaraie <robertjirsaraie21@gmail.com>
2020-07-13 19:39:21 -0700

    PIPELINE: Group ICA and Dual Regression using FSL Melodic

--- ToolBox/machine_learning/ica/pipeline_fsl_melodic.sh
+++ ToolBox/machine_learning/ica/pipeline_fsl_melodic.sh
@@ -6,13 +6,13 @@
 #$ -ckpt restart
 ################
 
-DIR_LOCAL_SCRIPTS=/dfs2/yassalab/rjirsara/ConteCenterScripts/ToolBox/bids_apps/xcpengine
-DIR_LOCAL_APPS=/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One/apps
-DIR_LOCAL_OUT=/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One/analyses/IntraFlux
-OPT_INCLUSION_FILE=/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One/audits/Audit_Master_ConteMRI.csv 
-OPT_INCLUSION_VAR=Inclusion_Cross
-OPT_TASK_LABEL=task-REST_run-01
-OPT_MANUAL_NCOMP=NULL
+DIR_TOOLBOX=/dfs2/yassalab/rjirsara/ConteCenterScripts/ToolBox
+DIR_PROJECT=/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One
+TEMPLATE_SPACE=$DIR_TOOLBOX/bids_apps/dependencies/atlases/tpl-MNI152NLin6Asym_res-02_desc-brain_mask.nii.gz
+TASK_LABELS="pipe-aroma_task-REST_run-1"
+OPT_MANUAL_NCOMPS=$(echo `seq 5 30` "pca")
+OPT_INCLUSION_FILE=${DIR_PROJECT}/datasets/aggregate_df.csv 
+OPT_INCLUSION_VAR=IntraFlux_Inclusion
 
 module purge ; module load anaconda/2.7-4.3.1 singularity/3.3.0 R/3.5.3 fsl/6.0.1
 source ~/Settings/MyPassCodes.sh
@@ -24,89 +24,110 @@ rm GroupICA.*
 ##### Read in Processed/Denoised fMRI Scans from XCPEngine Output Directory #####
 #################################################################################
 
-for DIR_PIPE in $DIR_LOCAL_APPS/xcpengine/fc-* ; do
-LABEL_PIPE=`basename $DIR_PIPE | sed s@'fc-'@''@g`
-ALL_SCANS=`find "$(pwd -P)" $DIR_PIPE -iname *_residualised.nii.gz`
-if [[ ! -z ${OPT_TASK_LABEL} && ${OPT_TASK_LABEL} == "task-"* ]] ; then
-LABELS_TASKS=${OPT_TASK_LABEL}
-else
-LABELS_TASKS=`echo ${ALL_SCANS} | tr '/' '\n' | grep ^task | sort -u` 
-fi
-for LABEL_TASK in ${LABELS_TASKS} ; do
-INPUT_SCANS=`echo $ALL_SCANS | tr ' ' '\n' | grep "/${LABEL_TASK}/"`
-DIR_OUTPUT="$DIR_LOCAL_OUT/GroupICA/fc-${LABEL_PIPE}_$(echo $LABEL_TASK | sed s@'task-'@'func-'@g)"
-TODAY=`date "+%Y%m%d"` ; mkdir -p ${DIR_OUTPUT} ${DIR_OUTPUT}/logs/${TODAY}
-COHORT_FILE=${DIR_OUTPUT}/logs/${TODAY}/n$(echo $INPUT_SCANS | wc -w)_Cohort_${LABEL_PIPE}.txt
-echo $INPUT_SCANS | tr ' ' '\n' > ${COHORT_FILE}
+TODAY=`date "+%Y%m%d"`
+for TASK_LABEL in $TASK_LABELS ; do
+	DIR_PIPE=$DIR_PROJECT/apps/xcp-fcon/${TASK_LABEL}
+	SCANS_PREPROC=`find $DIR_PIPE -iname *_residualised.nii.gz`
 
 ##########################################################
 ##### Select Only Subset of Input Scans If Requested #####
 ##########################################################
 
-if [[ -f ${OPT_INCLUSION_FILE} && ! -z `cat $OPT_INCLUSION_FILE | grep $OPT_INCLUSION_VAR` ]] ; then
-if [[ $(echo $INPUT_SCANS | tr ' ' '_') != *"ses-"* ]] ; then
-cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,${OPT_INCLUSION_VAR} | grep 1$ > $COHORT_FILE
-for ROW in `cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,${OPT_INCLUSION_VAR} | grep 1$` ; do
-SUB=`echo $ROW | cut -d ',' -f1`
-SCAN=`echo $INPUT_SCANS | tr ' ' '\n' | grep "/sub-${SUB}_"`
-NEW_ROW=`echo $ROW | sed s@${ROW}@${ROW},${SCAN}@g`
-cat ${COHORT_FILE} | sed s@${ROW}@${NEW_ROW}@g > ${COHORT_FILE}_NEW 
-mv ${COHORT_FILE}_NEW ${COHORT_FILE}
-done
-elif [[ $(echo $INPUT_SCANS | tr ' ' '_') == *"ses-"* ]] ; then
-cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,ses,${OPT_INCLUSION_VAR} | grep 1$ > $COHORT_FILE
-for ROW in `cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,ses,${OPT_INCLUSION_VAR} | grep 1$` ; do
-SUB=`echo $ROW | cut -d ',' -f1`
-SES=`echo $ROW | cut -d ',' -f2`
-SCAN=`echo $INPUT_SCANS | tr ' ' '\n' | grep "/sub-${SUB}_ses-${SES}_"`
-NEW_ROW=`echo $ROW | sed s@${ROW}@${ROW},${SCAN}@g`
-cat ${COHORT_FILE} | sed s@${ROW}@${NEW_ROW}@g > ${COHORT_FILE}_NEW 
-mv ${COHORT_FILE}_NEW ${COHORT_FILE}
-done
-else
-exit 0
-fi
-cat $COHORT_FILE | grep -v .nii.gz | cut -d ',' -f1,2 > ${COHORT_FILE}_MISSING
-cat $COHORT_FILE | grep .nii.gz | awk -F "\"*,\"*" '{print $4}' > ${COHORT_FILE}_SELECT
-MISS=`echo ${COHORT_FILE}_MISSING | sed s@n$(echo $INPUT_SCANS | wc -w)@n$(cat ${COHORT_FILE}_MISSING | wc  -l)@g`
-SELECT=`echo ${COHORT_FILE}_SELECT | sed s@n$(echo $INPUT_SCANS | wc -w)@n$(cat ${COHORT_FILE}_SELECT | wc  -l)@g`
-mv ${COHORT_FILE}_MISSING $(echo $MISS | sed s@'_MISSING'@''@g | sed s@'Cohort'@'Cohort_Missing'@g)
-mv ${COHORT_FILE}_SELECT $(echo $SELECT | sed s@'_SELECT'@''@g) ; rm ${COHORT_FILE} 
-COHORT_FILE=$(echo $SELECT | sed s@'_SELECT'@''@g)
-fi
+	if [[ -f ${OPT_INCLUSION_FILE} && ! -z `cat $OPT_INCLUSION_FILE | grep $OPT_INCLUSION_VAR` ]] ; then
+		if [[ $(echo $SCANS_PREPROC | tr ' ' '_') != *"ses-"* ]] ; then
+			cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,${OPT_INCLUSION_VAR} | grep 1$ > cohort.csv
+			for ROW in `cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,${OPT_INCLUSION_VAR} | grep 1$` ; do
+				SUB=`echo $ROW | cut -d ',' -f1`
+				SCAN=`echo $SCANS_PREPROC | tr ' ' '\n' | grep "/sub-${SUB}_"`
+				sed -i "s@^${ROW}@${SCAN}@g" cohort.csv
+			done
+		elif [[ $(echo $SCANS_PREPROC | tr ' ' '_') == *"ses-"* ]] ; then
+			cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,ses,${OPT_INCLUSION_VAR} | grep 1$ > cohort.csv
+			for ROW in `cat ${OPT_INCLUSION_FILE} |  csvcut -c sub,ses,${OPT_INCLUSION_VAR} | grep 1$` ; do
+				SUB=`echo $ROW | cut -d ',' -f1`
+				SES=`echo $ROW | cut -d ',' -f2`
+				SCAN=`echo $SCANS_PREPROC | tr ' ' '\n' | grep "/sub-${SUB}_ses-${SES}_"`
+				sed -i "s@^${ROW}@${SCAN}@g" cohort.csv
+				done
+		else
+			exit 0
+		fi
+		cat cohort.csv | grep .nii.gz > cohort_FINAL.csv 
+		echo "IF PREPROC SCANS ARE MISSING THEY WILL BE PRINTED HERE: `cat cohort.csv | grep -v .nii.gz`"
+		if [[ ! -z $OPT_INCLUSION_VAR && ${OPT_INCLUSION_VAR} == *'_Inclusion' ]] ; then
+			FILE_COHORT=$DIR_PIPE/group/n$(cat cohort_FINAL.csv | wc -l)_$(echo $OPT_INCLUSION_VAR | cut -d '_' -f1).grpica/logs/cohort_${TODAY}.csv
+		else
+			FILE_COHORT=$DIR_PIPE/group/n$(cat cohort_FINAL.csv | wc -l)_$(basename $DIR_PROJECT | cut -d '/' -f1).grpica/logs/cohort_${TODAY}.csv
+		fi
+		mkdir -p $(dirname $FILE_COHORT)
+		mv cohort_FINAL.csv $FILE_COHORT ; rm cohort.csv
+	fi
 
-##########################################################
-##### Select Only Subset of Input Scans If Requested #####
-##########################################################
+####################################################################################
+##### Define Project-Specific Arguements and Resample Input Images to Template #####
+####################################################################################
 
-if [[ -z "${OPT_MANUAL_NCOMP##[0-9]*}" || ${OPT_MANUAL_NCOMP} == "pca" ]]  ; then
-NCOMP_ITERATIONS=${OPT_MANUAL_NCOMP}
-else
-NCOMP_ITERATIONS=$(echo `seq 6 2 30` "pca")
-fi
-for NCOMP in $NCOMP_ITERATIONS ; do
-COMMAND_FILE=$(echo `dirname $COHORT_FILE`/dim-${NCOMP}_Command_${TODAY}.sh)
-LOG_FILE=`echo $COMMAND_FILE | sed s@"Command"@"Log"@g | sed s@'.sh'@'.txt'@g`
-ARG_NCOMP=$(echo -d ${NCOMP})
-if [[ $NCOMP == "pca" ]] ; then
-unset ARG_NCOMP
-fi
-echo "melodic -i ${COHORT_FILE} \
--o $DIR_OUTPUT/dim-${NCOMP} \
--m $FSLDIR/data/standard/MNI152_T1_2mm_brain_mask.nii.gz \
--a concat \
---report \
---Oall \
--v ${ARG_NCOMP}" | tr '\t' '#' | sed s@'#'@''@g > ${COMMAND_FILE}
+	if [[ -z "${OPT_MANUAL_NCOMP##[0-9]*}" || ${OPT_MANUAL_NCOMP} == "pca" ]]  ; then
+		NCOMP_ITERATIONS=${OPT_MANUAL_NCOMP}
+	else
+		NCOMP_ITERATIONS=$(echo `seq 12 1 32` "pca")
+	fi
+	if [[ ! -f TEMPLATE_SPACE ]] ; then
+		TEMPLATE_SPACE=$FSLDIR/data/standard/MNI152_T1_2mm_brain_mask.nii.gz
+	fi
+	module load mrtrix/3.0_RC3
+	for NIFTI in $SCANS_PREPROC ; do
+		FILE_DIM1=$(fslinfo $NIFTI | grep ^dim1 | awk {'print $2'})
+		FILE_DIM2=$(fslinfo $NIFTI | grep ^dim2 | awk {'print $2'})
+		FILE_DIM3=$(fslinfo $NIFTI | grep ^dim3 | awk {'print $2'})
+		TMPL_DIM1=$(fslinfo $TEMPLATE_SPACE | grep ^dim1 | awk {'print $2'})
+		TMPL_DIM2=$(fslinfo $TEMPLATE_SPACE | grep ^dim2 | awk {'print $2'})
+		TMPL_DIM3=$(fslinfo $TEMPLATE_SPACE | grep ^dim3 | awk {'print $2'})
+		if [[ $FILE_DIM1 != $TMPL_DIM1 || $FILE_DIM2 != $TMPL_DIM2 || $FILE_DIM3 != $TMPL_DIM3 ]] ; then
+			mrresize -size ${TMPL_DIM1},${TMPL_DIM2},${TMPL_DIM3} ${NIFTI} ${NIFTI} -force
+		fi
+	done
 
-chmod ug+wrx ${COMMAND_FILE}
+##################################################
+##### Execute FSL MELODIC Group ICA Commands #####
+##################################################
 
-${COMMAND_FILE} > ${LOG_FILE} 2>&1
+	GRPICA_COMMAND=$(echo `dirname $FILE_COHORT`/command_grpica_${TODAY}.sh)
+	GRPICA_LOG=`echo $GRPICA_COMMAND | sed s@"command"@"log"@g | sed s@'.sh'@'.txt'@g`
+	DUALREG_COMMAND=$(echo `dirname $FILE_COHORT`/command_dualreg_${TODAY}.sh)
+	DUALREG_LOG=`echo $DUALREG_COMMAND | sed s@"command"@"log"@g | sed s@'.sh'@'.txt'@g`
+	for NCOMP in $NCOMP_ITERATIONS ; do
+		ARG_NCOMP=$(echo -d ${NCOMP})
+		if [[ $NCOMP == "pca" ]] ; then
+			unset ARG_NCOMP
+		fi
 
-dual_regression melodic_oIC.nii.gz 1 -1 5000 ICA_temp.dr `cat ../logs/20200505/n145_Cohort_aroma.txt`
+		echo "melodic -i ${FILE_COHORT} \
+			-o `dirname $FILE_COHORT | sed s@'logs'@"dim-${NCOMP}"@g` \
+			-m $TEMPLATE_SPACE \
+			-a concat \
+			--report \
+			--Oall \
+			--nobet \
+			--mmthresh=0.5 \
+			-v ${ARG_NCOMP}" | tr '\t' '#' | sed s@'#'@''@g > ${GRPICA_COMMAND}
 
-done
-done
+		chmod ug+wrx ${GRPICA_COMMAND}
+
+		${GRPICA_COMMAND} > ${GRPICA_LOG} 2>&1
+
+		NIFTI_GRPICA_OUTPUT=$(echo `dirname $FILE_COHORT` | sed s@'logs'@"dim-${NCOMP}/melodic_IC.nii.gz"@g)
+		DIR_DUALREG_OUTPUT=$(dirname $FILE_COHORT | sed s@'grpica/logs'@"dualreg/dim-${NCOMP}"@g)
+		mkdir -p $DIR_DUALREG_OUTPUT
+
+		echo "dual_regression $NIFTI_GRPICA_OUTPUT 1 -1 50000 $DIR_DUALREG_OUTPUT `cat $FILE_COHORT | tr '\n' ' ' | grep -v local`" > ${DUALREG_COMMAND}
+
+		chmod ug+wrx ${DUALREG_COMMAND} ; unset SGE_ROOT
+
+		${DUALREG_COMMAND} > ${DUALREG_LOG} 2>&1
+
+		chmod -R ug+wrx $(echo $DIR_DUALREG_OUTPUT | sed s@'.dualreg'@'.*'@g)
+	done
 done
 
 ###################################################################################################
