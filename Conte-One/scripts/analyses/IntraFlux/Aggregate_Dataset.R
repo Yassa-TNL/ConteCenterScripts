2b31d266b996e94c66d53e069bbb5909657c6622
Robert Jisaraie <robertjirsaraie21@gmail.com>
2020-12-18 04:27:59 -0800

    Update

--- Conte-One/scripts/analyses/IntraFlux/Aggregate_Dataset.R
+++ Conte-One/scripts/analyses/IntraFlux/Aggregate_Dataset.R
@@ -2,7 +2,7 @@
 ######################
 
 print("Reading Arguments")
-DIR_PROJECT="/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One"
+DIR_PROJECT="/dfs7/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One"
 suppressMessages(require(mgcv))
 suppressMessages(require(visreg))
 suppressMessages(require(svglite))
@@ -118,8 +118,123 @@ for (DIM in list.files(path=paste0(DIR_PROJECT,"/analyses/IntraFlux/n138_IntraFl
 	write.csv(AMG,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_NetStr_AMG.csv"),row.names=FALSE)
 	write.csv(REST2,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_NetStr_REST2.csv"),row.names=FALSE)
 	write.csv(FIGURE,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_NetStr_Longitudinal.csv"),row.names=FALSE)
-
 }
+
+####################################################
+##### Find and Extract ALFF Values Per Network #####
+####################################################
+
+BASE_DIR=paste0(DIR_PROJECT,"/analyses/IntraFlux/n138_IntraFlux.alff/") ; REST1<-MDD ; AMG<-MDD ; REST2<-MDD
+REST1[,c("FD_MEAN_AMG","FD_MEAN_REST2")]<-NULL ; names(REST1)[7] <- "FD_MEAN"
+AMG[,c("FD_MEAN_REST1","FD_MEAN_REST2")]<-NULL ; names(AMG)[7] <- "FD_MEAN"
+REST2[,c("FD_MEAN_REST1","FD_MEAN_AMG")]<-NULL ; names(REST2)[7] <- "FD_MEAN"
+for (INDEX in 1:length(list.files(path=BASE_DIR,full.names=T, recursive=T,pattern=".csv"))){
+	FILE<-list.files(path=BASE_DIR,full.names=T, recursive=T,pattern=".csv")[INDEX]
+	print(paste0("WORKING: ",basename(FILE)))
+	SUBID<-gsub("sub-","",unlist(strsplit(basename(FILE),"_"))[1]) 
+	LABEL<-gsub(".csv","_ALFF",gsub("net-","COMP",unlist(strsplit(basename(FILE),"_"))[2]))
+	if (INDEX == 1){
+		REST1[,LABEL]<-NA
+		AMG[,LABEL]<-NA
+		REST2[,LABEL]<-NA
+	}
+	NROW=which(REST1$sub == SUBID)
+	REST1[NROW,LABEL]<-as.numeric(read.csv(FILE,header=F)[1,1])
+	AMG[NROW,LABEL]<-as.numeric(read.csv(FILE,header=F)[2,1])
+	REST2[NROW,LABEL]<-as.numeric(read.csv(FILE,header=F)[3,1])
+}
+REST1$TASK<-"REST1"
+AMG$TASK<-"AMG"
+REST2$TASK<-"REST2"
+LONG<-rbind(REST1,AMG,REST2)
+write.csv(REST1,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_ALFF_REST1.csv"),row.names=FALSE)
+write.csv(AMG,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_ALFF_AMG.csv"),row.names=FALSE)
+write.csv(REST2,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_ALFF_REST2.csv"),row.names=FALSE)
+write.csv(LONG,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_ALFF_Longitudinal.csv"),row.names=FALSE)
+
+#########################################################
+##### Calculate the Standarad Deviation Per Network #####
+#########################################################
+
+BASE_DIR=paste0(DIR_PROJECT,"/analyses/IntraFlux/n138_IntraFlux.time/") ; REST1<-MDD ; AMG<-MDD ; REST2<-MDD
+REST1[,c("FD_MEAN_AMG","FD_MEAN_REST2")]<-NULL ; names(REST1)[7] <- "FD_MEAN"
+AMG[,c("FD_MEAN_REST1","FD_MEAN_REST2")]<-NULL ; names(AMG)[7] <- "FD_MEAN"
+REST2[,c("FD_MEAN_REST1","FD_MEAN_AMG")]<-NULL ; names(REST2)[7] <- "FD_MEAN"
+for (INDEX in 1:length(list.files(path=BASE_DIR,full.names=T, recursive=T,pattern=".csv"))){
+	FILE<-list.files(path=BASE_DIR,full.names=T, recursive=T,pattern=".csv")[INDEX]
+	print(paste0("WORKING: ",basename(FILE)))
+	SUBID<-gsub("sub-","",unlist(strsplit(basename(FILE),"_"))[1]) 
+	LABEL<-gsub(".csv","_SD",gsub("net-","COMP",unlist(strsplit(basename(FILE),"_"))[2]))
+	if (INDEX == 1){
+		REST1[,LABEL]<-NA
+		AMG[,LABEL]<-NA
+		REST2[,LABEL]<-NA
+	}
+	NROW=which(REST1$sub == SUBID)
+	REST1[NROW,LABEL]<-sd(read.table(FILE,header=F)[1,],na.rm=T)
+	AMG[NROW,LABEL]<-sd(read.table(FILE,header=F)[2,],na.rm=T)
+	REST2[NROW,LABEL]<-sd(read.table(FILE,header=F)[3,],na.rm=T)
+}
+REST1$TASK<-"REST1"
+AMG$TASK<-"AMG"
+REST2$TASK<-"REST2"
+LONG<-rbind(REST1,AMG,REST2)
+write.csv(REST1,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_TIME_REST1.csv"),row.names=FALSE)
+write.csv(AMG,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_TIME_AMG.csv"),row.names=FALSE)
+write.csv(REST2,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_TIME_REST2.csv"),row.names=FALSE)
+write.csv(LONG,paste0(DIR_PROJECT,"/analyses/IntraFlux/Aggregate_ALFF_Longitudinal.csv"),row.names=FALSE)
+
+#########################################################
+##### Calculate the Standarad Deviation Per Network #####
+#########################################################
+
+TIMESERIES <- data.frame(matrix(NA,nrow=0,ncol=9))
+load(paste0(DIR_PROJECT,"/analyses/IntraFlux/GrowthCurveModeling/Final_LGCM_results.Rdata"))
+colnames(TIMESERIES)<-c("sub","AgeAtScan","Gender","PreMood_Ent","PreMood_Lvl","scl.CDI_MD","class.rchg.2.comp6","BOLD","Volume")
+for (FILE in list.files(paste0(DIR_PROJECT,"/analyses/IntraFlux/n138_IntraFlux.time"),full.names=T,pattern="6_concat.csv")){
+	SUBID<-gsub("sub-","",unlist(strsplit(basename(FILE),"_"))[1]) ; REFINE<-data2[which(data2$sub == SUBID),][1,]
+	REFINE<-REFINE[,c("sub","AgeAtScan","Gender","PreMood_Ent","PreMood_Lvl","scl.CDI_MD","class.rchg.2.comp6")]
+	REFINE<-REFINE[rep(1,430),] ; row.names(REFINE)<-NULL
+	TIME<-as.data.frame(t(suppressWarnings(read.table(FILE))))
+	TIME[,"Volume"]<-as.numeric(gsub("V","",row.names(TIME)))
+	row.names(TIME)<-NULL ; FINAL<-cbind(REFINE,TIME) ; names(FINAL)[8]<-"BOLD"
+	TIMESERIES<-rbind(TIMESERIES,FINAL) 
+}
+
+TIMESERIES$class.rchg.2.comp6<-as.factor(TIMESERIES$class.rchg.2.comp6)
+ggplot(TIMESERIES, aes(x=Volume,y=BOLD,group=sub,color=class.rchg.2.comp6)) + geom_line(size=0.2,alpha=0.8) + geom_abline(intercept=0,slope=0) + theme_classic() + scale_color_manual(values=c("#000000","#FF0000"))
+
+TIMESERIES$COLOR<-0
+TIMESERIES[which(TIMESERIES$Volume < 151),"COLOR"]<-1
+TIMESERIES[which(TIMESERIES$Volume > 150),"COLOR"]<-2
+TIMESERIES[which(TIMESERIES$Volume > 280),"COLOR"]<-3
+TIMESERIES$COLOR<-as.factor(TIMESERIES$COLOR)
+ggplot(TIMESERIES, aes(x=Volume,y=BOLD,group=sub,color=COLOR)) + geom_line(size=0.1,alpha=0.8) + geom_abline(intercept=0,slope=0) + theme_classic() + facet_wrap(~class.rchg.2.comp6) + scale_color_manual(values=c("#0000FF","#FF0000","#008000"))
+
+###
+
+CONTENT$VARIABLE_CLUSTER<-1
+for (SUBID in unique(TIMESERIES$sub)){
+	CONTENT[which(CONTENT$sub == SUBID),"VARIABLE_CLUSTER"]<-data2[which(data2$sub == SUBID)[1],"class.rchg.2.comp6"]
+}
+CONTENT[which(CONTENT$VARIABLE_CLUSTER==2),"VARIABLE_CLUSTER"]<-0
+CONTENT$VARIABLE_CLUSTER<-as.factor(CONTENT$VARIABLE_CLUSTER)
+
+CONTENT$TIME_SD<-0 ; CONTENT$ALFF_MEAN<-0
+ALFF<-read.csv("/dfs7/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One/analyses/IntraFlux/Aggregate_ALFF_Longitudinal.csv")
+for (FILE in list.files(paste0(DIR_PROJECT,"/analyses/IntraFlux/n138_IntraFlux.time"),full.names=T,pattern="6_concat.csv")){
+	SUBID<-gsub("sub-","",unlist(strsplit(basename(FILE),"_"))[1]) 
+	CONTENT[which(CONTENT$sub==SUBID),"TIME_SD"]<-sd(t(suppressWarnings(read.table(FILE)))[,1])
+	CONTENT[which(CONTENT$sub==SUBID),"ALFF_MEAN"]<-mean(ALFF[which(ALFF$sub == SUBID),"COMP6_ALFF"])
+}
+t.test(CONTENT$ALFF_MEAN~CONTENT$VARIABLE_CLUSTER) ; t.test(CONTENT$ALFF_MEAN~CONTENT$VARIABLE_CLUSTER)
+ggplot(CONTENT,aes(x=TIME_SD,y=brainage_DBN,group=sub))
+
+ggplot(CONTENT, aes(x=TIME_SD, fill=VARIABLE_CLUSTER)) + geom_density(alpha=.8) +scale_fill_manual(values=c("#FF0000","#000000")) + theme_classic()
+ggplot(CONTENT, aes(x=ALFF_MEAN, fill=VARIABLE_CLUSTER)) + geom_density(alpha=.8) +scale_fill_manual(values=c("#FF0000","#000000")) + theme_classic()
+
+write.csv(CONTENT,"/dfs7/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One/analyses/IntraFlux/Aggregate_MASTER_20201216.csv",row.names=F)
+write.csv(TIMESERIES,"/dfs7/dfs2/yassalab/rjirsara/ConteCenterScripts/Conte-One/analyses/IntraFlux/Aggregate_TIME_20201216.csv",row.names=F)
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
